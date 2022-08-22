32bc94e66807f6857c17ea19dc79e2f87d4929e3
aleksanderwalczuk <ohleg@windowslive.com>
2022-08-22 18:42:56 +0300

    add player state save

--- src/stores/player.ts
+++ src/stores/player.ts
@@ -1,10 +1,11 @@
-import { defineStore } from 'pinia';
+import { defineStore, type PiniaCustomStateProperties } from 'pinia';
 
 export const usePlayerStore = defineStore({
   id: 'player',
   state: () => ({
     score: 0,
     name: 'Untitled',
+    level: 0,
   }),
   getters: {
     doubleCount: (state) => state.score * 2,
@@ -18,6 +19,27 @@ export const usePlayerStore = defineStore({
     },
     setName(name: string) {
       this.name = name;
+    },
+    loadState() {
+      if (window.localStorage != null) {
+        return JSON.parse(localStorage.getItem('memoGameState') ?? '[]') as Array<unknown>;
+      }
+      throw Error('localStorage is undefined');
+    },
+    setState(state: PiniaCustomStateProperties<{ score?: number, name?: string, level?: number}>) {
+      this.$patch(state);
+    },
+    saveState() {
+      if (window.localStorage != null) {
+        const savedGameStates = JSON.parse(localStorage.getItem('memoGameState') ?? '[]') as Array<unknown>;
+        const gameState = {
+          time: Date.now(),
+          player: this.name,
+          state: this.$state,
+        };
+
+        window.localStorage.setItem('memoGameState', JSON.stringify([...savedGameStates, gameState]));
+      }
     },
   },
 });
