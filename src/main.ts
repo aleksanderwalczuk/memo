72ca8f83b630f10c77f631cce2af8a304883f0ef
aleksanderwalczuk <ohleg@windowslive.com>
2022-09-03 11:20:38 +0200

    add Formkit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { plugin, defaultConfig } from '@formkit/vue';
 
 import App from './App.vue';
 import router from './router/index';
@@ -12,5 +13,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(plugin, defaultConfig);
 
 app.mount('#app');
