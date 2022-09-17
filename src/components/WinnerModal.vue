3707339d6c9c9d63a36c02baef1904ede9e284c4
aleksanderwalczuk <ohleg@windowslive.com>
2022-09-17 13:14:10 +0200

    add basic formkit form to winnerModal

--- src/components/WinnerModal.vue
+++ src/components/WinnerModal.vue
@@ -10,15 +10,33 @@ const playerStore = usePlayerStore();
 const modalClosed = ref(false);
 
 const isOpen = computed(() => gameStore.hasWon && modalClosed.value === false);
+
+function handleSubmit(formData: {name: string}) {
+  const data = { ...formData, score: playerStore.score };
+  console.log(data);
+  modalClosed.value = true;
+}
 </script>
 
 <template>
   <TheModal :is-open="isOpen" @close="modalClosed = true">
     <div v-if="gameStore.hasWon">
       <h2>Congratulations! You've won the game 🎉</h2>
-      <form class="flex flex-col">
-        Fill the form below to proceed
-      </form>
+      <div class="flex justify-between">
+        <div class="w-1/2">
+          <FormKit @submit="handleSubmit" type="form">
+            <FormKit label="Player name" name="name" :value="playerStore.name" />
+          </FormKit>
+        </div>
+        <div class="w-1/2">
+          <div class="scoreboard w-1/2">
+          <h3>Highscores:</h3>
+          <ol>
+            <li class="flex justify-between"><span>Some user</span><span>5</span></li>
+          </ol>
+        </div>
+        </div>
+      </div>
     </div>
   </TheModal>
 </template>
